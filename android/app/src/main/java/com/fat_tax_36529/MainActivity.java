879800d4e7a3f4297db8c4e341c31b829cb94c00
Crowdbotics <team@crowdbotics.com>
2022-07-31 14:54:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fat_tax_36529/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fat_tax_36529;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "fat_tax_36529";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fat_tax_36529",
+  url: "https://fat_tax_36529.botics.co",
+  api: "https://fat_tax_36529.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
